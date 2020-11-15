70e33a72234decec2ea25f4fde5973a234421649
Melvin <marchenko1985g@mail.ru>
2020-11-15 23:05:58 +0100

    home

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,23 +1,29 @@
 <template>
-    <div class="m-to">
+    <div class="m-t-w">
  <div class="container">
   <img src="/images/v.png" alt="Avatar" style="width:90px">
   <p><span>Vue.js</span></p>
-  <p>HTML CSS saved us from a web disaster.</p>
+  <p>Изучаю этот фреймворк и получаю удовольствие от его простоты.</p>
+  <p>Он развивается и предпологаю, что он еще себя проявит.</p>
+  <p>Реализует все возможности для создания frontEnd.</p>
+  <p>Vue + Laravel считаю лучшей связкой инструментов.</p>
 </div>
 
 <div class="container">
   <img src="/images/laravel.png" alt="Laravel" style="width:90px">
   <p><span >Laravel</span></p>
-  <p>Я использую этот фреймворк потомучто считаю, что он хорошо продуман.</p>
+  <p>Я использую этот фреймворк потому что считаю, что он хорошо продуман.</p>
+  <p>Уже с коробки предоставляет уйму мощных возможностей.</p>
+  <p>Мощное средство для разработки backEnd.</p>
 </div> 
     </div>
 </template>
 
-<style scoped>
-.m-to{
+<style>
+.m-t-w{
        margin-top: 10%;
-        margin-left: 5%;
+       width: 900px; 
+         margin-left: 15%;
 
 }
  /* Style the container with a rounded border, grey background and some padding and margin */
